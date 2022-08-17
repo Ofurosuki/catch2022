e54b9df716910e423e20d7e02efb29b889848f39
Oyaki <oc.yaki122@gmail.com>
2022-08-17 14:35:23 +0900

    cobs送受信を実装

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <CanManager.h>
 #include <Motor.h>
+#include <PC.h>
 #include <Sensor.h>
 #include <Servo.h>
 #include <Solenoid.h>
@@ -7,22 +8,22 @@
 
 asm(".global _printf_float");  // float出力用
 
-UnbufferedSerial pc(USBTX, USBRX);
+UnbufferedSerial pc(USBTX, USBRX, 115200);
 
 RawCAN can(PA_11, PA_12, 500E3);
 CanManager manager(can);
 Motor motor(0x08, manager);
+PC pcConnector;
 
+DigitalOut led(LED1);
 DigitalIn button(BUTTON1);
 
 int main() {
   manager.begin();
-  printf("start\n");
-  motor.setMode(Motor::Mode::Position);
-  motor.drivePosition(4);
-
+  pcConnector.registerCallback(0x01, [&](uint8_t* data, size_t size) {
+    led = !led;
+    asm("nop");
+  });
   while (true) {
-    printf("pos: %f, %f%%\n", motor.getCurrentPosition(),
-           motor.getPositionProgress() * 100);
   }
 }
