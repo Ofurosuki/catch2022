a53bb2c1632ed313c45a98eb7920163b8d2b1a02
Ofurosuki <ryo-yoshida@keio.jp>
2022-08-25 16:30:34 +0900

    added examples in main

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 #include <config.h>
 #include <mbed.h>
 #include <stepper.h>
+#include <stepper_rotate.h>
 
 asm(".global _printf_float");  // float出力用
 
@@ -23,10 +24,13 @@ int main() {
   motor.setMode(Motor::Mode::Position);
   motor.drivePosition(4);
 
-  Stepper stepper0(DIR0, STP0);
-  stepper0.set_config(3, 50);  // set acceleration and  max velocity
-  stepper0.step(20,
-                -1000);  // rotate stepper (initial frequency and target step )
+  rotate_stepper stepper0(DIR0, STP0);
+  stepper0.set_config(3, 50);  // set acceleration and  max velocity  
+  stepper0.step(20,-1000);  // rotate stepper (initial frequency and target step )　　回すだけの関数もある
+
+  stepper0.set_theta_config(0,0.5); //起動時の角度を指定、1ステップあたりの角度を指定
+
+  stepper0.rotate(20,50.0);   //rotate stepper by angle (initial frequency and angle)
 
   while (true) {
     printf("pos: %f, %f%%\n", motor.getCurrentPosition(),
