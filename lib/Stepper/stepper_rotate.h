4edf1e4ae3dd13e6de014b1a1eb0a526fd94a947
Ofurosuki <ryo-yoshida@keio.jp>
2022-08-25 16:41:28 +0900

    moved config.h into stepper_lib

--- lib/Stepper/stepper_rotate.h
+++ lib/Stepper/stepper_rotate.h
@@ -2,15 +2,16 @@
 #define __STEPPER_ROTATE_H__
 #include "mbed.h"
 #include "stepper.h"
-class rotate_stepper: public Stepper{
-    float step_per_theta;
-    float theta_zero;
-    int step_mode=1;
-    public:
-    void rotate(int freq_ini,float theta);
-    float progress_cnt();
-    void set_theta_config(float theta_0,float theta_per_stp);
-    void set_step_mode(int num);
-    rotate_stepper(PinName dir,PinName stp);
+class rotate_stepper : public Stepper {
+  float step_per_theta = 10;
+  float theta_zero = 0;
+  int step_mode = 1;
+
+ public:
+  void rotate(int freq_ini, float theta);
+  float progress_cnt();
+  void set_theta_config(float theta_0, float theta_per_stp);
+  void set_step_mode(int num);
+  rotate_stepper(PinName dir, PinName stp);
 };
 #endif
