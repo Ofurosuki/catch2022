ab33bfc5232527c9100043a5964bc00146e63492
Oyaki122 <oc.yaki122@gmail.com>
2022-09-05 15:18:42 +0900

    gui受信を修正

--- lib/Gui/Gui.cpp
+++ lib/Gui/Gui.cpp
@@ -1,12 +1,14 @@
 #include "Gui.h"
 
 void Gui::pcCallback(uint8_t* data, size_t size) {
-  config.getFrom = data[0];
-  config.moveTo = data[1];
-  config.enableSuckers[2] = data[2] & 0b0001;
-  config.enableSuckers[2] = data[2] & 0b0010;
-  config.enableSuckers[2] = data[2] & 0b0100;
+  if (size == 0 || size > 6) return;
+  if (data[0] > 2) return;
+  config.mode = (CommandMode)data[0];
+  config.destination = data[1];
   config.isHigher = data[2] & 0b1000;
+  config.enableSuckers[0] = data[3];
+  config.enableSuckers[1] = data[4];
+  config.enableSuckers[2] = data[5];
 
   newConfig = true;
   if (callback == nullptr) return;
--- lib/Gui/Gui.h
+++ lib/Gui/Gui.h
@@ -4,19 +4,24 @@
 
 class Gui {
  public:
-  struct GuiConfig {
-    uint8_t getFrom;
-    uint8_t moveTo;
+  enum CommandMode {
+    ownArea = 0,
+    commonArea = 1,
+    shootingBox = 2,
+  };
+  struct Command {
+    CommandMode mode;
+    uint8_t destination;
     bool enableSuckers[3];
     bool isHigher;
   };
 
   Gui() {}
-  void registerCallback(Callback<void(GuiConfig)> func) { callback = func; }
+  void registerCallback(Callback<void(Command)> func) { callback = func; }
   void removeCallback() { callback = nullptr; }
   void pcCallback(uint8_t* data, size_t size);
 
-  GuiConfig getConfig() { return config; }
+  Command getConfig() { return config; }
 
   bool checkNewConfig() {
     if (!newConfig) return false;
@@ -25,7 +30,7 @@ class Gui {
   }
 
  private:
-  Callback<void(GuiConfig)> callback;
-  GuiConfig config;
+  Callback<void(Command)> callback;
+  Command config;
   bool newConfig = false;
 };
