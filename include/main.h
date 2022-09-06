885e1382023b344942ea2223866ce34159693cad
Ofurosuki <ryo-yoshida@keio.jp>
2022-09-07 00:46:01 +0900

    added function for delivering jaga

--- include/main.h
+++ include/main.h
@@ -1,6 +1,12 @@
 #pragma once
 #include <stepper_rotate.h>
+#include <Motor.h>
+#include <Solenoid.h>
+#include <Servo.h>
 
 extern rotate_stepper stepper_z;
 extern rotate_stepper stepper_theta;
 extern rotate_stepper stepper_r;
+extern Motor motor;
+extern Servo servo;
+extern Solenoid solenoid;
--- src/move_them.cpp
+++ src/move_them.cpp
@@ -4,9 +4,15 @@
 #include "field_data.h"
 #include "main.h"
 
-void move(position pos) {
+void move(position pos, bool is_common = 0) {
   float r = sqrt((pos.x - pos.x_1) * (pos.x - pos.x_1) + pos.y * pos.y);
   stepper_r.rotate(cal_theta(pos));
+  motor.drivePosition(pos.x_1);  //何回転が何ミリか確認し、修正
+  if (is_common) {
+    servo.setPosition(90.0 - cal_theta(pos));
+  } else {
+    servo.setPosition(jaga_3_degree - cal_theta(pos));
+  }
 }
 
 float cal_theta(position pos) {
@@ -21,4 +27,11 @@ float cal_theta(position pos) {
   }
 }
 void catch_jaga(float z) { stepper_z.rotate(z); }
-void release_jaga(z_height z) {}
+void release_jaga(bool sucker0 = 1, bool sucker1 = 1, bool sucker2 = 1) {
+  if (sucker0) solenoid.driveSingle(0, 1, 1000);  // 1を吸引解除ということに
+  if (sucker1) solenoid.driveSingle(1, 1, 1000);
+  if (sucker2) solenoid.driveSingle(2, 1, 1000);
+}
+
+void take_down(float z) { stepper_z.rotate(z); }
+void take_up(float z) { stepper_z.rotate(z); }
