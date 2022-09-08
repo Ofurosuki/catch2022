e431e0c3498c1767ae73d6c82944b9202391ad7a
Ofurosuki <ryo-yoshida@keio.jp>
2022-09-09 00:14:12 +0900

    fixed bugs and explain

--- lib/movethem/move_them.h
+++ lib/movethem/move_them.h
@@ -8,6 +8,7 @@ void release_jaga(bool sucker0 = 1, bool sucker1 = 1, bool sucker2 = 1);
 void take_down(float z);
 void take_up();
 void gamepad_input_to_command();
+float phi(int phi);
 void getDegree();
 
 extern const int delta_time_to_resuck;
