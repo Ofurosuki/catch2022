5fe885cc296fc7724076da4f93e88be349ad680a
Oyaki <oc.yaki122@gmail.com>
2022-09-11 18:57:24 +0900

    マニュアル操縦を更新

--- lib/movethem/move_them.cpp
+++ lib/movethem/move_them.cpp
@@ -28,11 +28,12 @@ float cal_theta(position pos) {
 }
 
 void move(position pos, float phi = 45.0f) {
-  if(pos!=here){
-  float r = sqrt((pos.x - pos.x_1) * (pos.x - pos.x_1) + pos.y * pos.y);
-  stepper_theta.rotate(cal_theta(pos));
-  stepper_r.rotate(r);
-  move_x1(pos.x_1);}
+  if (pos != here) {
+    float r = sqrt((pos.x - pos.x_1) * (pos.x - pos.x_1) + pos.y * pos.y);
+    stepper_theta.rotate(cal_theta(pos));
+    stepper_r.rotate(r);
+    move_x1(pos.x_1);
+  }
   servo.setPosition(phi - cal_theta(pos));
 }
 
@@ -51,55 +52,71 @@ float joyDeg0;
 float joyDeg1;
 int step_choice;  // switch文用。どのステッピングモーターを動かすか
 
-void gamepad_input_to_command() {
-  while (gamepad.getButton(1) == 0) {
-    const float DCVelocity = 10;
-    int StepVel1 = (gamepad.getAxis(1)) * 5;
-    int StepVel2 = (gamepad.getAxis(2)) * 5;
-    int StepVel3 = (gamepad.getAxis(3)) * 5;
-    if (abs(gamepad.getAxis(0)) >= 10) {
-      // Dead zone
-      // stepXを動かす
-      motor.driveVelocity(DCVelocity);
-      if (abs(joyDeg0) <= M_PI / 12 || abs(joyDeg0) >= (M_PI / 12) * 11) {
-        // stepxを動かす
-        motor.driveVelocity(DCVelocity);
-      }
-    } else if (abs(gamepad.getAxis(1)) >= 10) {
-      // Dead zone
-      // stepシータを動かす
-      stepper_theta.rotate_vel(StepVel1);
-      if (abs(joyDeg0) >= (M_PI / 12) * 5 && abs(joyDeg0) <= (M_PI / 12) * 7) {
-        // stepθを+に動かす
-        // gamepad.Axis(1)の値はマイナスなので正負入れ替えたほうがいいかも
-        stepper_theta.rotate_vel(StepVel1);
-      }
-    } else {
-      // stepper止める
-      stepper_theta.rotate_vel(0);
-    }
-
-    //サーボモーターを動かす
-    const float ServoVelocity = 5;
-    while (gamepad.getButton(4) == 1) {
-      //反時計回り
-      //サーボの角度＝サーボの角度+5°
-      //みたいな感じで単発押しと長押しで角度調整できるようにする。
-      servo.setVelocity(-ServoVelocity);
-    }
-
-    while (gamepad.getButton(5) == 1) {
-      //時計回り
-      servo.setVelocity(ServoVelocity);
-    }
-  }
+void getDegree() {
+  int xAxis0 = gamepad.getAxis(0);
+  int yAxis0 = gamepad.getAxis(1);
+  int xAxis1 = gamepad.getAxis(2);
+  int yAxis1 = gamepad.getAxis(3);
+  joyDeg0 = atan2(yAxis0, xAxis0);
+  joyDeg1 = atan2(yAxis1, xAxis1);
 }
 
-void getDegree(int xAxis0, int yAxis0, int xAxis1, int yAxis1) {
-  xAxis0 = gamepad.getAxis(0);
-  yAxis0 = gamepad.getAxis(1);
-  xAxis1 = gamepad.getAxis(2);
-  yAxis1 = gamepad.getAxis(3);
-  joyDeg0 = atan2(xAxis0, yAxis0);
-  joyDeg1 = atan2(xAxis1, yAxis1);
+void gamepad_input_to_command() {
+  while (gamepad.getButton(1) == 0) {
+    getDegree();
+    const float DCVelocity = (float)gamepad.getAxis(0) / 100;
+    int StepVel1 = (gamepad.getAxis(1)) * 2;
+    int StepVel2 = (gamepad.getAxis(2)) * 2;
+    int StepVel3 = (gamepad.getAxis(3));
+
+    // stepXを動かす
+    if ((abs(joyDeg0) <= M_PI / 12 || abs(joyDeg0) >= (M_PI / 12) * 11) &&
+        abs(gamepad.getAxis(0)) >= 10) {
+      // stepxを動かす
+      motor.driveVoltage(DCVelocity);
+      stepper_theta.rotate_vel(0);
+    }
+    // stepシータを動かす
+    else if ((abs(joyDeg0) >= (M_PI / 12) * 5 &&
+              abs(joyDeg0) <= (M_PI / 12) * 7) &&
+             abs(gamepad.getAxis(1)) >= 10) {
+      stepper_theta.rotate_vel(StepVel1);
+      motor.driveVoltage(0);
+    } else {
+      // stepper, DC止める
+      stepper_theta.rotate_vel(0);
+      motor.driveVoltage(0);
+    }
+
+    // stepRを動かす
+    if ((abs(joyDeg1) <= M_PI / 12 || abs(joyDeg1) >= (M_PI / 12) * 11) &&
+        abs(gamepad.getAxis(2)) >= 10) {
+      // stepRを動かす
+      stepper_r.rotate_vel(StepVel2);
+      stepper_z.rotate_vel(0);
+    }
+    // step上下を動かす
+    else if ((abs(joyDeg1) >= (M_PI / 12) * 5 &&
+              abs(joyDeg1) <= (M_PI / 12) * 7) &&
+             (abs(gamepad.getAxis(3)) >= 10)) {
+      // step上下に動かす
+      stepper_z.rotate_vel(StepVel3);
+      stepper_r.rotate_vel(0);
+    } else {
+      // stepper止める
+      stepper_r.rotate_vel(0);
+      stepper_z.rotate_vel(0);
+    }
+    //サーボモーターを動かす
+    const float ServoVelocity = 50;  // deg/s
+    if (gamepad.getButton(4) == 1) {
+      //反時計回り
+      servo.setVelocity(-ServoVelocity);
+    } else if (gamepad.getButton(5) == 1) {
+      //時計回り
+      servo.setVelocity(ServoVelocity);
+    } else {
+      servo.setVelocity(0);
+    }
+  }
 }
