9fee208cbd76d84e23b9494a59280ea30e73bc72
Oyaki <oc.yaki122@gmail.com>
2022-09-11 10:35:12 +0900

    動いた

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,7 @@ UnbufferedSerial pc(USBTX, USBRX);
 
 RawCAN can(PA_11, PA_12, 500E3);
 CanManager manager(can);
-Motor motor(0x08, manager);
+Motor motor(0x01, manager);
 
 DigitalIn button(BUTTON1);
 
@@ -19,13 +19,15 @@ int main() {
   manager.begin();
   printf("start\n");
   float counter = 0;
+
+  motor.drivePosition(-4);
   while (true) {
     printf("pos: %f, %f%%\n", motor.getCurrentPosition(),
            motor.getPositionProgress() * 100);
-    motor.setMode(Motor::Mode::Voltage);
-    motor.driveVoltage(counter);
-    counter += 0.1;
-    if (counter >= 1) counter = -1;
+    // motor.setMode(Motor::Mode::Voltage);
+    if (!button) {
+      motor.reset();
+    }
     ThisThread::sleep_for(100ms);
   }
 }
