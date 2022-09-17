b385ce8a53f7a04054c59e78efd557291e278554
Oyaki <oc.yaki122@gmail.com>
2022-09-18 00:27:17 +0900

    最大速度を上昇

--- src/main.cpp
+++ src/main.cpp
@@ -46,7 +46,7 @@ void initialize(Team team) {
   stepper_theta.set_config(7, 100, 5);
   stepper_theta.set_max_vel_diff(0.2);
   stepper_z.set_theta_config(0, 481.0f / 154.0f);
-  stepper_z.set_config(40, 150, 10);
+  stepper_z.set_config(40, 300, 10);
 
   const int stepper_vel_for_init = 10;
   const float motor_voltage_for_init = 0.15;
